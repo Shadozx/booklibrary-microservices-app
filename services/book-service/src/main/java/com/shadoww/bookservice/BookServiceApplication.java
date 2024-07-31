b11774b329bdbfe3dc4c52ab96c97bcae69fdc6c
Shadozx <matkivskiy761@gmail.com>
2024-07-31 15:51:27 +0300

    fix ports in docker compose

--- services/book-service/src/main/java/com/shadoww/bookservice/BookServiceApplication.java
+++ services/book-service/src/main/java/com/shadoww/bookservice/BookServiceApplication.java
@@ -2,10 +2,12 @@ package com.shadoww.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.shadoww")
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
